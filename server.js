b23f2c97b0b513bca05d9478eb3b85f99d05a539
aabazary <85041715+aabazary@users.noreply.github.com>
2023-05-16 02:06:58 -0700

    Updated Server connection added dotenv

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGODBPATH, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
